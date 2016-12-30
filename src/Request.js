f10a208b9383ffc25bc7ad657c45dfa149628bbc
Christoph Herrmann <christoph.herrmann@maxdome.de>
2016-12-30 14:53:42 +0100

    Provide express like API and using middlewares for AWS Lambda

---
+++ src/Request.js
@@ -0,0 +1,21 @@
+class Request {
+  constructor(event) {
+    if (event.body) {
+      this.body = JSON.parse(event.body);
+    }
+    this.method = event.httpMethod;
+
+    /*
+     * Headers are case insensitive. Node lower case all headers in the 'http'
+     * module which is not in use for lambda, but a feature request is
+     * contributed to serverless:
+     * https://github.com/serverless/serverless/issues/2765
+     */
+    this.headers = {};
+    Object.keys(event.headers).forEach((key) => {
+      this.headers[key.toLowerCase()] = event.headers[key];
+    });
+  }
+}
+
+module.exports = Request;
---
+++ src/Response.js
@@ -0,0 +1,55 @@
+class Response {
+  constructor(context) {
+    this.body = '';
+    this.context = context;
+    this.headers = {};
+    this.statusCode = 204;
+  }
+
+  set(key, value) {
+    this.headers[key] = value;
+    return this;
+  }
+
+  setHeader(key, value) {
+    this.headers[key] = value;
+    return this;
+  }
+
+  get(key) {
+    return this.headers[key];
+  }
+
+  getHeader(key) {
+    return this.headers[key];
+  }
+
+  removeHeader(key) {
+    delete this.headers[key];
+    return this;
+  }
+
+  status(statusCode) {
+    this.statusCode = statusCode;
+    return this;
+  }
+
+  send(body) {
+    if (typeof body !== 'string') {
+      this.body = JSON.stringify(body);
+    }
+    this.end();
+    return this;
+  }
+
+  end() {
+    this.context.succeed({
+      body: this.body,
+      headers: this.headers,
+      status: this.statusCode
+    });
+    return this;
+  }
+}
+
+module.exports = Response;
