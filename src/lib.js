914cf852e37cff4a1a75df330eb268751ac5462f
Christoph Herrmann <christoph.herrmann@maxdome.de>
2017-02-01 11:06:04 +0100

    Transform to node 4 and don't use this for objects

--- src/lib.js
+++ src/lib.js
@@ -1,32 +1,34 @@
 const Request = require('./Request');
 const Response = require('./Response');
 
-module.exports = logger => ({
-  middlewares: [],
-  use: (middleware) => {
-    this.middlewares.push(middleware);
-  },
-  function: (name, controller) => async (event, context) => {
-    const req = new Request(event);
-    logger.debug(`Request for '${name}' with headers '${JSON.stringify(req.headers)}' and body '${JSON.stringify(req.body)}'`);
-    const res = new Response(context);
-    try {
-      this.middlewares.forEach(async (middleware) => {
-        await new Promise((resolve, reject) => {
-          middleware(req, res, (err) => {
-            if (err) {
-              reject(err);
-            } else {
-              resolve();
-            }
+module.exports = logger => {
+  const middlewares = [];
+  return {
+    use: (middleware) => {
+      middlewares.push(middleware);
+    },
+    function: (name, controller) => async (event, context) => {
+      const req = new Request(event);
+      logger.debug(`Request for '${name}' with headers '${JSON.stringify(req.headers)}' and body '${JSON.stringify(req.body)}'`);
+      const res = new Response(context);
+      try {
+        middlewares.forEach(async (middleware) => {
+          await new Promise((resolve, reject) => {
+            middleware(req, res, (err) => {
+              if (err) {
+                reject(err);
+              } else {
+                resolve();
+              }
+            });
           });
         });
-      });
-      await controller(req, res);
-    } catch (e) {
-      logger.warn(`Error: ${e.message}`);
-      res.status(400).send(e.message);
+        await controller(req, res);
+      } catch (e) {
+        logger.warn(`Error: ${e.message}`);
+        res.status(400).send(e.message);
+      }
+      logger.debug(`Response for '${name}' with statusCode '${res.statusCode}', headers '${JSON.stringify(res.headers)}' and body '${JSON.stringify(res.body)}'`);
     }
-    logger.debug(`Response for '${name}' with statusCode '${res.statusCode}', headers '${JSON.stringify(res.headers)}' and body '${JSON.stringify(res.body)}'`);
-  }
-});
+  };
+};
