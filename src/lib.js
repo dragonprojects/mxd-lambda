f30607157b2864e8cf6b519fac9b77ebe6e1524c
Christopher Marchfelder <christopher.marchfelder@maxdome.de>
2017-03-02 12:03:22 +0100

    Reworked middleware handling to behave more like express.js one's for compatibility

--- src/lib.js
+++ src/lib.js
@@ -1,34 +1,40 @@
 const Request = require('./Request');
 const Response = require('./Response');
 
-module.exports = logger => {
-  const middlewares = [];
-  return {
-    use: (middleware) => {
-      middlewares.push(middleware);
-    },
-    function: (name, controller) => async (event, context) => {
-      const req = new Request(event);
-      logger.debug(`Request for '${name}' with headers '${JSON.stringify(req.headers)}' and body '${JSON.stringify(req.body)}'`);
-      const res = new Response(context);
-      try {
-        middlewares.forEach(async (middleware) => {
-          await new Promise((resolve, reject) => {
-            middleware(req, res, (err) => {
-              if (err) {
-                reject(err);
-              } else {
-                resolve();
-              }
-            });
-          });
-        });
-        await controller(req, res);
-      } catch (e) {
-        logger.warn(`Error: ${e.message}`);
-        res.status(400).send(e.message);
-      }
-      logger.debug(`Response for '${name}' with statusCode '${res.statusCode}', headers '${JSON.stringify(res.headers)}' and body '${JSON.stringify(res.body)}'`);
+const stack = [];
+const use = middleware => stack.push(middleware);
+
+const handle = (name, controller) => async (event, context) => {
+  const req = new Request(event);
+  const res = new Response(context);
+
+  // A controller is also a middleware
+  const stackWithController = stack;
+  // stackWithController.push(async (req, res, next) => await controller(req, res));
+
+  // Index for the current middleware
+  let idx = -1;
+
+  await next();
+  async function next(error) {
+    if (error) {
+      return res.status(400).send(error);
     }
-  };
+
+    idx++;
+    if (idx >= stackWithController.length) {
+      // We are done, work from inner to outer again
+      return;
+    }
+
+    await stackWithController[idx](req, res, next);
+  }
+
+  // We need to call a end() again otherwise the response fail's
+  res.end();
+};
+
+module.exports = {
+  use,
+  handle
 };
